276583252b662db28b64fcb3c7fbc65b309a07c4
SAI UPESH <isaiupesh@gmail.com>
2026-05-07 16:16:04 +0530

    Update register_file2.v

--- src/register_file/register_file2.v
+++ src/register_file/register_file2.v
@@ -1,36 +1,31 @@
-
 module register_file2(
     input clk,
-
     input write_enable1,
     input write_enable2,
-
     input [4:0] write_address1,
     input [4:0] write_address2,
-
     input [7:0] write_data1,
     input [7:0] write_data2,
-
     input [4:0] read_address1,
     input [4:0] read_address2,
-
     output [7:0] data_out1,
     output [7:0] data_out2
 );
-
 reg [7:0] registers[31:0];
+
+integer i;
+initial begin
+    for(i = 0; i < 32; i = i + 1)
+        registers[i] = 8'd0;
+end
 
 assign data_out1 = registers[read_address1];
 assign data_out2 = registers[read_address2];
 
 always @(posedge clk) begin
-
     if(write_enable1)
         registers[write_address1] <= write_data1;
-
     if(write_enable2)
         registers[write_address2] <= write_data2;
-
 end
-
 endmodule
